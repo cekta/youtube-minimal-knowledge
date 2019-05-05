a1d92a1eb5911506b06dd354d241a2aa04983e71
Evgeniy Kuvshinov <smpl@itmywork.com>
2019-05-05 17:39:44 +0500

    Настройка CORS

--- api/public/index.php
+++ api/public/index.php
@@ -1,4 +1,10 @@
 <?php
 declare(strict_types=1);
 
-echo 'hello world';
+// CORS
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, FETCH');
+
+header('Content-Type: application/json');
+echo json_encode(['message' => 'hello world']);
+
