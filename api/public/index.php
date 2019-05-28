fff11bb0386b6bc948267c83f928a8fc8ebd9b24
Evgeniy Kuvshinov <smpl@itmywork.com>
2019-05-28 18:39:30 +0500

    Request, Response, Server, OOP fix https://github.com/cekta/youtube-minimal-knowledge/issues/13

--- api/public/index.php
+++ api/public/index.php
@@ -1,21 +1,14 @@
 <?php
 declare(strict_types=1);
 
-// CORS
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST, GET, OPTIONS, FETCH');
+use Cekta\YoutubeMinimalKnowledge\Server;
+use Zend\Diactoros\ServerRequestFactory;
+use Zend\HttpHandlerRunner\Emitter\SapiEmitter;
 
-header('Content-Type: application/json');
+require __DIR__ . '/../vendor/autoload.php';
 
-if ($_SERVER['REQUEST_URI'] === '/' && $_SERVER['REQUEST_METHOD'] === 'GET') {
-    echo json_encode(['message' => 'hello world']);
-} elseif ($_SERVER['REQUEST_URI'] === '/message' && $_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Здесь отправку на почту
-    mail(getenv('MAIL_TO'), 'Сообщение с сайта', "ИМЯ: {$_POST['name']} \n Телефон: {$_POST['phone']}");
-    echo json_encode(['message' => 'message was sent']);
-} else {
-    http_response_code(404);
-    echo json_encode(['message' => 'page not found']);
-}
-
-
+$server = new Server();
+$request = ServerRequestFactory::fromGlobals();
+$response = $server->handle($request);
+$emitter = new SapiEmitter();
+$emitter->emit($response);
